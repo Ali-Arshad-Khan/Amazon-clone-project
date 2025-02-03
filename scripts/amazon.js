3858f25f3c017c5b37fd2df9f849b61a2d83c4b7
Ali Arshad Khan <aliarshadkhan02@gmail.com>
2025-02-03 23:25:18 +0530

    Make search bar interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart, addToCart, calculateCartQuantity} from '../data/cart.js'; 
+import {cart, addToCart} from '../data/cart.js';
 import {products, loadProducts} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -7,111 +7,100 @@ loadProducts(renderProductsGrid);
 function renderProductsGrid() {
   let productsHTML = '';
 
-  products.forEach((product) => {
-      productsHTML += `
-          <div class="product-container">
-            <div class="product-image-container">
-              <img class="product-image"
-                src="${product.image}">
-            </div>
+  const url = new URL(window.location.href);
+  const search = url.searchParams.get('search');
 
-            <div class="product-name limit-text-to-2-lines">
-              ${product.name}
-            </div>
+  let filteredProducts = products;
 
-            <div class="product-rating-container">
-              <img class="product-rating-stars"
-                src="${product.getStarsUrl()}">
-              <div class="product-rating-count link-primary">
-                ${product.rating.count}
-              </div>
-            </div>
+  // If a search exists in the URL parameters,
+  // filter the products that match the search.
+  if (search) {
+    filteredProducts = products.filter((product) => {
+      return product.name.includes(search);
+    });
+  }
 
-            <div class="product-price">
-            ${product.getPrice()}
-            </div>
+  filteredProducts.forEach((product) => {
+    productsHTML += `
+      <div class="product-container">
+        <div class="product-image-container">
+          <img class="product-image"
+            src="${product.image}">
+        </div>
 
-            <div class="product-quantity-container">
-              <select class="js-quantity-selector-${product.id}">
-                <option selected value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-              </select>
-            </div>
+        <div class="product-name limit-text-to-2-lines">
+          ${product.name}
+        </div>
 
-            ${product.extraInfoHTML()}
-
-            <div class="product-spacer"></div>
-
-            <div class="added-to-cart js-added-to-cart-${product.id}">
-              <img src="images/icons/checkmark.png">
-              Added
-            </div>
-
-            <button class="add-to-cart-button button-primary js-add-to-cart"
-            data-product-id="${product.id}">
-              Add to Cart
-            </button>
+        <div class="product-rating-container">
+          <img class="product-rating-stars"
+            src="${product.getStarsUrl()}">
+          <div class="product-rating-count link-primary">
+            ${product.rating.count}
           </div>
-      `;
-      
+        </div>
+
+        <div class="product-price">
+          ${product.getPrice()}
+        </div>
+
+        <div class="product-quantity-container">
+          <select>
+            <option selected value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+
+        ${product.extraInfoHTML()}
+
+        <div class="product-spacer"></div>
+
+        <div class="added-to-cart">
+          <img src="images/icons/checkmark.png">
+          Added
+        </div>
+
+        <button class="add-to-cart-button button-primary js-add-to-cart"
+        data-product-id="${product.id}">
+          Add to Cart
+        </button>
+      </div>
+    `;
   });
 
-
-  document.querySelector('.js-products-grid').
-  innerHTML = productsHTML;
-
-
-  const addedMessageTimeouts = {};
+  document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
   function updateCartQuantity() {
-    
-    const cartQuantity = calculateCartQuantity();
+    let cartQuantity = 0;
 
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+    });
 
-          document.querySelector('.js-cart-quantity').
-          innerHTML = cartQuantity;
-  };
-
-  function addedToCartMessage(productId) {
-    const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`)
-        
-          addedMessage.classList.add('visible');
-
-          // product. If there is, we should stop it.
-          const previousTimeoutId = addedMessageTimeouts[productId];
-          if (previousTimeoutId) {
-              clearTimeout(previousTimeoutId);
-          }
-
-          const timeoutId = setTimeout(() => {
-              addedMessage.classList.remove('visible');
-          }, 2000);
-
-          // Save the timeoutId for this product
-          // so we can stop it later if we need to.
-          addedMessageTimeouts[productId] = timeoutId;
-  };
+    document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
+  }
 
   document.querySelectorAll('.js-add-to-cart')
-      .forEach((button) => {
-          button.addEventListener('click', () => {
-          const { productId } = button.dataset;
-        
-          const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
-          const selectorValue = Number(quantitySelector.value);    
-          addToCart(productId,selectorValue);
-          updateCartQuantity();
-          addedToCartMessage(productId);
-          });
+    .forEach((button) => {
+      button.addEventListener('click', () => {
+        const productId = button.dataset.productId;
+        addToCart(productId);
+        updateCartQuantity();
       });
+    });
 
-  updateCartQuantity();    
+  document.querySelector('.js-search-button')
+    .addEventListener('click', () => {
+      const search = document.querySelector('.js-search-bar').value;
+      window.location.href = `amazon.html?search=${search}`;
+    });
 }
